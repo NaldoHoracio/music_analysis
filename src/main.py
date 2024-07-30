f53c1a853988964157de3a83f9d263e52fb7a886
EdHoracio <edvonaldohoracio@gmail.com>
2024-07-30 17:15:35 -0300

    Add gitignore

---
+++ src/main.py
@@ -0,0 +1,25 @@
+"""
+Created on Tue Jul 30 16:49:01 2024
+
+@author: edhoracio
+"""
+
+def test_function(param1: int):
+    '''
+
+    Parameters
+    ----------
+    param1 : int
+        DESCRIPTION.
+
+    Returns
+    -------
+    param1
+
+    '''
+
+    print("Hello world, Ed!")
+
+
+if __name__ == '__main__':
+    test_function(param1=3)
